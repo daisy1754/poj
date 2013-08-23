7f11d240ce1bb22fbfa6ead546f7d3cdda2a1bb9
daisy1754 <d-part@hotmail.co.jp>
2013-08-23 21:43:44 +0900

    update template class with Solver inner class

--- src/Template.java
+++ src/Template.java
@@ -6,5 +6,20 @@ import java.util.Scanner;
 public class Template {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Solver solver;
+        while ((solver = newSolverWithScannedInput(scanner)) != null) {
+            System.out.println(solver.resultAsString());
+        }
+    }
+
+    private static Solver newSolverWithScannedInput(Scanner scanner) {
+        return null;
+    }
+
+    private static class Solver {
+
+        public String resultAsString() {
+            return "";
+        }
     }
 }
